857806a4a320e3e21068ad964552dab4556fed58
Nitish kumar <nitish.bpns@gmail.com>
2024-02-02 01:12:56 +0530

    login signup navbar

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import balloon from "./../../../public/assets/balloon.png";
+import Link from "next/link";
 
 function page() {
   return (
-    <div>This is the login page</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.col1}>
+        <Image src={balloon} alt="img" className={styles.img1} />
+      </div>
+      <div className={styles.col2}>
+        <div className={styles.card}>
+          <div className={styles.head1}>Login to your account</div>
+          <label className={styles.input}>Email</label>
+          <br />
+          <input placeholder="Enter your Email" className={styles.inpEmail} />
+          <br />
+          <label className={styles.input}>Password</label>
+          <br />
+          <input
+            placeholder="Enter your Password"
+            className={styles.inpEmail}
+          />
+          <center>
+            <Link href="/">
+              <button className={styles.loginBtn}>Login</button>
+            </Link>
+          </center>
+
+          <div className={styles.txt}>
+            Don't have a account?{" "}
+            <Link href="/signup">
+              <span className={styles.txt2}>Sign Up</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default page;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,73 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+import logo from "./../../../public/assets/logo.png";
+import hamburger from "./../../../public/assets/hamburger.png";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className={styles.container}>
+      <Link href={"/"}>
+        <Image
+          src={logo}
+          className={styles.img}
+          width={150}
+          style={{ height: "auto" }}
+          placeholder="blur"
+          alt="Logo"
+        />
+      </Link>
+      <div className={styles.menu}>
+        <Link className={styles.link} href="/#about">
+          How is works
+        </Link>
+        <Link href="/signup">
+            <button  className={styles.link3}>Sign Up</button>
+          
+        </Link>
+        <Link  href="/login">
+          <button className={styles.link2}>Login</button>
+        </Link>
+      </div>
+      <Image
+        className={styles.hamburger}
+        src={hamburger}
+        width={20}
+        style={{ height: "auto" }}
+        alt="Hamburger"
+        onClick={() => setOpen(!open)}
+      />
+      {open && (
+        <div className={styles.menu2}>
+          <Link
+            className={styles.link}
+            href="/#about"
+            onClick={() => setOpen(!open)}
+          >
+            About
+          </Link>
+          <Link
+            className={styles.link}
+            href="/#products"
+            onClick={() => setOpen(!open)}
+          >
+            Products
+          </Link>
+          <Link
+            className={styles.link}
+            href="/#contact"
+            onClick={() => setOpen(!open)}
+          >
+            Contact
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
