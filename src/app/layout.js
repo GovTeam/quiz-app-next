a5c186b6684dd137d296a7505f004245d8a776f3
Anoop Singh <93050936+AnoopSingh07@users.noreply.github.com>
2024-02-02 02:24:21 +0530

    added tailwind and made the landing page

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className + " px-12 "}>
         <Navbar />
         {children}
       </body>
