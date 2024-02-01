03e07bab36b9ff06386d6f68f19c1025e2ac8deb
Anoop Singh <93050936+AnoopSingh07@users.noreply.github.com>
2024-02-02 02:54:11 +0530

    fixed nav issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,9 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className + " px-12 "}>
+      <body className={inter.className}>
         <Navbar />
-        {children}
+         {children}
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -5,21 +5,26 @@ import styles from "./page.module.css";
 export default function Home() {
   console.log(styles);
   return (
-    <div className="relative container mx-auto pt-[200px]">
-      <img className="absolute top-0 pointer-events-none hidden lg:block" src="./HeroImg.svg"/>
-      <div>
-        <h1 className="text-5xl font-semibold text-gray-800">
-          Learn <br/>
-          new concepts <br/>
-          for each question
-        </h1>
-        <h2 className={" mt-16 text-xl font-normal text-gray-400 border-l-[1px] ml-[4px] py-2 px-3 border-gray-900 "}>
-          We help you prepare for exams and quizes 
-        </h2>
-        <div className="mt-16 flex items-center gap-8">
-          <a href="/questions" className=" bg-primary shadow-xl shadow-primary/50 text-white font-semibold p-4 ">
-            Start Solving
-          </a>
+    <div className="px-12">
+      <div className="relative container mx-auto pt-[200px]">
+        <img className="absolute top-0 pointer-events-none hidden lg:block" src="./HeroImg.svg"/>
+        <div>
+          <h1 className="text-5xl font-semibold text-gray-800">
+            Learn <br/>
+            new concepts <br/>
+            for each question
+          </h1>
+          <h2 className={" mt-16 text-xl font-normal text-gray-400 border-l-[1px] ml-[4px] py-2 px-3 border-gray-900 "}>
+            We help you prepare for exams and quizes 
+          </h2>
+          <div className="mt-16 flex items-center gap-8">
+            <a href="/questions" className=" bg-primary shadow-xl shadow-primary/50 text-white font-semibold p-4 ">
+              Start Solving
+            </a>
+            <a href="/about" className=" text-primary font-normal">
+              ▼ know more
+            </a>
+          </div>
         </div>
       </div>
     </div>
