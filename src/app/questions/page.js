60668a6cd3f450215fae29a1dc8ffb6a5b5a7086
Nitish kumar <nitish.bpns@gmail.com>
2024-02-03 01:41:52 +0530

    add match the following

--- src/app/questions/page.js
+++ src/app/questions/page.js
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 import BottomPane from "@/components/bottomPane/BottomPane";
 
 function page() {
-  let types = "select";
+  let types = "match";
 
   return (
     <div className={styles.container}>
@@ -64,6 +64,34 @@ function page() {
                   value="Submit"
                 />
               </form>
+            </center>
+          </div>
+        )}
+
+        {/* Type:Select */}
+        {types === "match" && (
+          <div>
+            <center>
+              <div className={styles.qno}>Ques no:</div>
+              <div className={styles.head2}>Match the pairs</div>
+              <div className={styles.match1}>
+                <div className={styles.matchCol1}>
+                  {/* <p className={styles.col1t}>Column A</p> */}
+                  <p className={styles.col2t}>A. Bird</p>
+                  <p className={styles.col2t}>B. Lion</p>
+                  <p className={styles.col2t}>C. Human</p>
+                  <p className={styles.col2t}>D. Monkey</p>
+                  <p className={styles.col2t}>E. Fish</p>
+                </div>
+                <div className={styles.matchCol2}>
+                  {/* <p className={styles.col1t}>Column B</p> */}
+                  <p className={styles.col2t}>1. Water</p>
+                  <p className={styles.col2t}>2. Nest</p>
+                  <p className={styles.col2t}>3. Trees</p>
+                  <p className={styles.col2t}>4. House</p>
+                  <p className={styles.col2t}>5. Den</p>
+                </div>
+              </div>
             </center>
           </div>
         )}
