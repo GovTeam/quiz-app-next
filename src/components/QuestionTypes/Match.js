e9b990c06a2158eeea0f87cafafe209c38105115
MUCCHU <harshwasnik160602@gmail.com>
2024-02-03 03:42:28 +0530

    integrated Match question type

--- src/components/QuestionTypes/Match.js
+++ src/components/QuestionTypes/Match.js
@@ -1,27 +1,26 @@
 import React from "react";
 import styles from './page.module.css';
-function Match(){
+function Match(props){
+    const {question} = props;
     return (
         <div>
         <center>
           <div className={styles.qno}>Ques no:</div>
-          <div className={styles.head2}>Match the pairs</div>
+          <div className={styles.head2}>{question.question}</div>
           <div className={styles.match1}>
             <div className={styles.matchCol1}>
-              {/* <p className={styles.col1t}>Column A</p> */}
-              <p className={styles.col2t}>A. Bird</p>
-              <p className={styles.col2t}>B. Lion</p>
-              <p className={styles.col2t}>C. Human</p>
-              <p className={styles.col2t}>D. Monkey</p>
-              <p className={styles.col2t}>E. Fish</p>
+              {question.options.left.map((option, index) => {
+                return(
+                  <p key={index} className={styles.col2t}>{option}</p>
+                )
+              })}
             </div>
             <div className={styles.matchCol2}>
-              {/* <p className={styles.col1t}>Column B</p> */}
-              <p className={styles.col2t}>1. Water</p>
-              <p className={styles.col2t}>2. Nest</p>
-              <p className={styles.col2t}>3. Trees</p>
-              <p className={styles.col2t}>4. House</p>
-              <p className={styles.col2t}>5. Den</p>
+            {question.options.right.map((option, index) => {
+                return(
+                  <p key={index} className={styles.col2t}>{option}</p>
+                )
+              })}
             </div>
           </div>
         </center>
