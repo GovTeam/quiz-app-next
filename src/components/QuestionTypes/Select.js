c467454e186ebfe475ecdf21a47373652fea056f
MUCCHU <harshwasnik160602@gmail.com>
2024-02-03 19:22:25 +0530

    saved answers displayed for select type questions

--- src/components/QuestionTypes/Select.js
+++ src/components/QuestionTypes/Select.js
@@ -1,10 +1,11 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import styles from './page.module.css';
 import { setAnswer } from "@/Redux/features/ansSlice";
-import { useAppDispatch } from "@/Redux/hooks";
+import { useAppDispatch, useAppSelector } from "@/Redux/hooks";
 
 function Select(props){
     const {question} = props;
+    const answers = useAppSelector(state => state.answers.answers);
     const [selected, setSelected] = useState('');
     const dispatch = useAppDispatch();
     const handleSubmit = (e) => {
@@ -12,6 +13,11 @@ function Select(props){
         console.log(selected);
         dispatch(setAnswer({index: question.order, value: selected}));
     }
+    useEffect(() => {
+      if(answers[question.order].answered){
+        setSelected(answers[question.order].value);
+      }
+    }, [answers])
 
     return(        
     <div>
@@ -30,7 +36,7 @@ function Select(props){
                     id="age1"
                     name="age"
                     value={option}
-                    selected = {selected === option}
+                    checked = {selected == option}
                     onChange={(e) => setSelected(e.target.value)}
                   />
                   <label className={styles.label}>{option}</label>
