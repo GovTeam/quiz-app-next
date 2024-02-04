c7e747aad9d94930faf88fe0db150803e5aa1d99
MUCCHU <harshwasnik160602@gmail.com>
2024-02-04 10:26:34 +0530

    added results page

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import StoreProvider from "./StoreProvider";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
+      </Head>
       <body className={inter.className}>
         <StoreProvider>
           <Navbar />
--- src/app/result/page.jsx
+++ src/app/result/page.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import trophy from '@assets/Results/gymklogo.png';
+import Image from 'next/image';
+import Card from '@/components/Card/Card';
+import styles from './page.module.css';
 
 function page() {
   return (
-    <div>This is the result page</div>
+    <div className="container py-6 px-10">
+      <h1 className="text-4xl text-center">Results</h1>
+      {/* <h1 className="text-2xl py-7">Here's a summary of the quiz you attempted </h1> */}
+      <div className="flex flex-row gap-10 justify-center py-10">
+      <Card heading="9" description="Total Questions" />
+      <Card heading="7" description="Attempted" />
+      <Card heading="6" description="Correct" />
+      </div>
+      <div className='flex flex-row justify-center py-7'> <button className={styles.saveBtn}> View correct answers </button> </div>
+      {/* <div className='flex flex-row justify-center'> <Image src={trophy} /> </div> */}
+    </div>
   )
 }
 
