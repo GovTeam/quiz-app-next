0acea9f52078775664ed085c9f1e8a35a184efc7
Nitish kumar <nitish.bpns@gmail.com>
2024-02-04 11:16:51 +0530

    update match UI

--- src/app/fill-blanks/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function page() {
-  return <div>This page is for fill in the blanks</div>;
-}
-
-export default page;
--- src/components/QuestionTypes/Match.js
+++ src/components/QuestionTypes/Match.js
@@ -1,30 +1,39 @@
 import React from "react";
-import styles from './page.module.css';
-function Match(props){
-    const {question} = props;
-    return (
-        <div>
-        <center>
-          <div className={styles.qno}>Ques no:</div>
-          <div className={styles.head2}>{question.question}</div>
-          <div className={styles.match1}>
-            <div className={styles.matchCol1}>
-              {question.options.left.map((option, index) => {
-                return(
-                  <p key={index} className={styles.col2t}>{option}</p>
-                )
-              })}
-            </div>
-            <div className={styles.matchCol2}>
-            {question.options.right.map((option, index) => {
-                return(
-                  <p key={index} className={styles.col2t}>{option}</p>
-                )
-              })}
-            </div>
+import styles from "./page.module.css";
+function Match(props) {
+  const { question } = props;
+  console.log(question.options.left);
+  return (
+    <div>
+      <center>
+        <div className={styles.qno}>Ques no:</div>
+        <div className={styles.head2}>{question.question}</div>
+        <div className={styles.instru} >Please enter the correct option in the blank provided.</div>
+        <div className={styles.match1}>
+          <div className={styles.matchCol1}>
+            {question.options.left.map((option, index) => {
+              return (
+                <div className={styles.inpCol}>
+                  <p key={index} className={styles.col2t}>
+                    {option}
+                  </p>
+                  <input type="text" maxLength={1} className={styles.colInput} />
+                </div>
+              );
+            })}
           </div>
-        </center>
-      </div>
-    )
+          <div className={styles.matchCol2}>
+            {question.options.right.map((option, index) => {
+              return (
+                <p key={index} className={styles.col3t}>
+                  {option}
+                </p>
+              );
+            })}
+          </div>
+        </div>
+      </center>
+    </div>
+  );
 }
 export default Match;
