9d424d50c58f0655b67581d4cc9c6ca2e1b1c19b
Anoop Singh <93050936+AnoopSingh07@users.noreply.github.com>
2024-02-05 20:14:46 +0530

    updated name

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -7,7 +7,7 @@ export default function Home() {
                 <img className=" absolute top-0 pointer-events-none hidden lg:block" src="./HeroImg.svg" />
                 <div className=" w-full lg:w-1/2">
                     <h2 className={" text-xl font-normal text-gray-400 border-l-[1px] ml-[4px] px-3 mb-[60px] border-gray-900 "}>
-                        At QuizTaker, we believe that learning should be an exciting adventure for kids! Our platform is designed to make education engaging, interactive, and, most importantly, fun for children of all ages. Whether they are exploring new concepts, reinforcing classroom learning, or just having a good time, our website offers a wide range of educational activities.
+                        At Bharat QuizTaker, we believe that learning should be an exciting adventure for kids! Our platform is designed to make education engaging, interactive, and, most importantly, fun for children of all ages. Whether they are exploring new concepts, reinforcing classroom learning, or just having a good time, our website offers a wide range of educational activities.
                     </h2>
                     <div class="grid grid-cols-2 gap-4">
                         <div class="bg-blue-500 p-4 font-semibold">
